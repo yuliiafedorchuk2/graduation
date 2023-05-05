1dd23bcb2274b9cc7dae5c6aabff54da7e845e8a
Yuliia <120779819+yuliiafedorchuk2@users.noreply.github.com>
2023-05-05 11:18:41 +0100

    add all files

---
+++ script.js
@@ -0,0 +1,48 @@
+function searchOnWebsite(query) {
+    const elements = document.getElementsByTagName('*');
+  
+    const results = [];
+  
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+  
+      if (element.innerText.includes(query)) {
+        results.push(element);
+      }
+    }
+    
+    return results;
+}
+  
+
+function displayResults(results) {
+    const container = document.getElementById('search-results');
+  
+    container.innerHTML = ''; 
+  
+    if (results.length === 0) {
+      container.innerText = 'No results found.';
+    } else {
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+  
+        const resultElement = document.createElement('div');
+        resultElement.innerText = result.innerText;
+  
+        container.appendChild(resultElement);
+      }
+    }
+}
+  
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const input = document.getElementById('search-input');
+    const query = input.value;
+  
+    const results = searchOnWebsite(query);
+  
+    displayResults(results);
+});  
