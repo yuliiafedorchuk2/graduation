b4bcb77f963fa5496f2217df09ce1745451fc761
Yuliia <120779819+yuliiafedorchuk2@users.noreply.github.com>
2023-05-05 21:20:36 +0100

    add all files

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-function searchOnWebsite(query) {
-    const elements = document.getElementsByTagName('*');
-  
-    const results = [];
-  
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-  
-      if (element.innerText.includes(query)) {
-        results.push(element);
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
       }
     }
-    
-    return results;
-}
-  
-
-function displayResults(results) {
-    const container = document.getElementById('search-results');
-  
-    container.innerHTML = ''; 
-
-    if (results.length === 0) {
-      container.innerText = 'No results found.';
-    } else {
-      for (let i = 0; i < results.length; i++) {
-        const result = results[i];
-  
-        const resultElement = document.createElement('div');
-        resultElement.innerText = result.innerText;
-  
-        container.appendChild(resultElement);
-      }
-    }
-}
-  
-
-const searchOnClick = function() {
-    const input = document.getElementById('search-input');
-    const query = input.value;
-  
-    const results = searchOnWebsite(query);
-  
-    displayResults(results);
-};  
+  });
